e02e7e3bcff2834f02550c21cc061bffa11fe9e5
yuoleynik <yuoleynik@edu.hse.ru>
2021-09-11 20:10:17 +0300

    Task_4

---
+++ 01 module/01 seminar/Hw_1/Task_4/Task_4/Task_4/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double U;
+            Console.WriteLine("Введите значение напряжения:");
+            double.TryParse(Console.ReadLine(), out U);
+            double R;
+            Console.WriteLine("Введите значение сопротивления:");
+            double.TryParse(Console.ReadLine(), out R);
+            double P = Math.Pow(U, 2) / R;
+            Console.WriteLine("Значение мощности: " + P);
+        }
+    }
+}
