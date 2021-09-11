4df4c9c71d10fb02e3d750f8d9cae0f0989c1ee8
yuoleynik <yuoleynik@edu.hse.ru>
2021-09-11 20:02:17 +0300

    ASCIIDecoder

---
+++ 01 module/01 seminar/Hw_1/ASCIIDecoder/ASCIIDecoder/ASCIIDecoder/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ASCIIDecoder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a;
+            Console.WriteLine("Введите номер искомого символа");
+            int.TryParse(Console.ReadLine(), out a);
+            char b = (char)a;
+            Console.WriteLine("Искомый символ: " + b);
+            
+        }
+    }
+}
