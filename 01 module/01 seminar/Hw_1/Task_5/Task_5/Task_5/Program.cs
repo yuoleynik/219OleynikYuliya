256b0aa38eb0c006eaa6cd56badd2c5a46727f86
yuoleynik <yuoleynik@edu.hse.ru>
2021-09-11 20:16:26 +0300

    Task_5

---
+++ 01 module/01 seminar/Hw_1/Task_5/Task_5/Task_5/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task_5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение первого катета: ");
+            double a;
+            double.TryParse(Console.ReadLine(), out a);
+            Console.WriteLine("Введите значение второго катета: ");
+            double b;
+            double.TryParse(Console.ReadLine(), out b);
+            double c = Math.Sqrt(Math.Pow(a, 2) + Math.Pow(b, 2));
+            Console.WriteLine("Значение гипотенузы: " + c);
+        }
+    }
+}
